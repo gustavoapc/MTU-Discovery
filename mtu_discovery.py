d5b5f853da3ffb89a857746c1195c4c3cf2682b9
Gustavo Porto <105179924+gustavoapc@users.noreply.github.com>
2024-02-27 22:32:21 -0300

    Update mtu_discovery.py

--- mtu_discovery.py
+++ mtu_discovery.py
@@ -1,18 +1,18 @@
 import os
 
-def test_mtu(destination, packet_size):
+def test_mtu(destination, max_packet_size):
     try:
-        # Executa o ping com o tamanho do pacote especificado
-        response = os.system(f"ping {destination} -l {packet_size} -f")
-        if response == 0:
-            print(f"MTU de {packet_size} bytes é suportado até {destination}.")
-        else:
-            print(f"MTU de {packet_size} bytes não é suportado até {destination}.")
+        for packet_size in range(1400, max_packet_size + 1, 10):
+            response = os.system(f"ping {destination} -l {packet_size} -f")
+            if response == 0:
+                print(f"MTU de {packet_size} bytes é suportado até {destination}.")
+            else:
+                print(f"MTU de {packet_size} bytes não é suportado até {destination}.")
     except Exception as e:
         print(f"Erro ao executar o teste: {e}")
 
 # Exemplo de uso
-destination_ip = "8.8.8.8"  # Substitua pelo destino desejado
-packet_size = 1461  # Tamanho do pacote em bytes
+destination_ip = "189.127.160.216"  # Substitua pelo destino desejado
+max_packet_size = 1500  # Tamanho máximo do pacote em bytes
 
-test_mtu(destination_ip, packet_size)
+test_mtu(destination_ip, max_packet_size)
